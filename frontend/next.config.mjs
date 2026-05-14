35cbbf380ec495a130855fc4dbddcff7991f27b0
Manumay Raj Mishra <manumay8@gmail.com>
2026-05-14 12:04:27 +0530

    disable eslint build checks

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,3 +4,11 @@ const nextConfig = {
 };
 
 export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
